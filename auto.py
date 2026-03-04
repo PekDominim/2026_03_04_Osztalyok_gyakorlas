ca449de3bc2e08e3ccce14612e0159b2a163f524
PekDominim <pek.dominik@diak.szbi-pg.hu>
2026-03-04 09:36:15 +0100

    Auto kezelés
    Átlag életkor, lidödősebb autó, rnd sebesség

--- auto.py
+++ auto.py
@@ -2,7 +2,19 @@ class Auto:
     def __init__(self, marka_, tipus_, gyartasi_ev):
         self.marka = marka_
         self.tipus = tipus_
-        self.gyartasi_ev = gyartasi_ev
+        self.gyartasi_ev = int(gyartasi_ev)
+        self.sebesseg = 0
 
     def __str__(self):
-        return f"{self.marka} {self.tipus} {self.gyartasi_ev}"
+        return f"{self.marka} {self.tipus} {self.gyartasi_ev}, sebesség:{self.sebesseg}"
+
+    def gyorsit(self, ertek):
+        self.sebesseg += ertek
+        if self.sebesseg > 200:
+            self.sebesseg = 200
+
+    def lassit(self, ertek):
+        self.sebesseg -= ertek
+        if self.sebesseg < 0:
+            self.sebesseg = 0
+            print("Megaltal")
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from auto import Auto
+import random
+import statistics
 
-auto1= Auto("Toyota", "Corolla", 2015)
-auto2= Auto("Ford", "Focus", 2018)
+auto1 = Auto("Toyota", "Corolla", 2015)
+auto2 = Auto("Ford", "Focus", 2018)
+auto3 = Auto("Audi", "E-tron", 2021)
+auto4 = Auto("Ford", "Mustang", 2005)
 
 print(auto1)
+print(auto2)
+
+auto1.gyorsit(300)
+auto1.lassit(400)
+
+autok = [auto1, auto2, auto3, auto4]
+for auto in autok:
+    print(auto)
+
+print("\nGyorsit\n")
+for auto in autok:
+    auto.gyorsit(random.randint(30, 150))
+    print(auto)
+
+gyartasi_evek = [auto.gyartasi_ev for auto in autok]
+kor = []
+for year in gyartasi_evek:
+    kor.append(2026 - int(year))
+print(statistics.mean(kor))
+
+for auto in autok:
+    if auto.gyartasi_ev == min(gyartasi_evek):
+        print(f"A legidösebb autó: {auto.marka} {auto.tipus} {2026-auto.gyartasi_ev}")
